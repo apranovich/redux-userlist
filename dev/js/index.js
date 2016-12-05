d49d5b315461cf56b19a7519ce05353bf943d275
apranovich <artyom.pranovich@gmail.com>
2016-12-05 07:56:45 +0300

    Initial commit

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,23 +1,20 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from "react-dom";
-import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-import promise from 'redux-promise';
-import createLogger from 'redux-logger';
-import allReducers from './reducers';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/index';
 import App from './components/App';
 
-const logger = createLogger();
-const store = createStore(
-    allReducers,
-    applyMiddleware(thunk, promise, logger)
+
+const store = createStore(rootReducer);
+
+const root = (
+	<Provider store={store}>
+	   <App/>
+	</Provider>
 );
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
+	root, document.getElementById('root')
 );
---
+++ dev/js/reducers/users.js
@@ -0,0 +1,7 @@
+export default function users(state = [], action = {}) {
+  return [
+    {id: 1, firstname: 'Artyom', lastname: 'Pranovich'}, 
+    {id: 2, firstname: 'Kost', lastname: 'Kalinousli'}, 
+    {id: 3, firstname: 'Alex', lastname: 'Kachura'}
+  ]
+}
